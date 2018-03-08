5b36090a5409d25f0311a1f836a03db962bf0391
caozhf-code <119944445@qq.com>
2018-03-08 20:38:06 +0800

    Second commit to my github

--- app/src/main/java/com/caozhf/gittest2/MainActivity.java
+++ app/src/main/java/com/caozhf/gittest2/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String bb = "ff";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
