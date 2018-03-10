72e4c59a1302294cddebc6f10dc14cb47b56f213
caozhf-code <119944445@qq.com>
2018-03-10 20:58:18 +0800

    add baiduMap

--- app/src/main/java/com/caozhf/gittest2/MainActivity.java
+++ app/src/main/java/com/caozhf/gittest2/MainActivity.java
@@ -1,69 +1,83 @@
 package com.caozhf.gittest2;
 
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ContextMenu;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.PopupWindow;
-import android.widget.Spinner;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
 
-    private Spinner spinner;
-    private String[] arr = new String[]{"a","b","c"};
-    private Button Dialog;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    MapView mapView = null;
+    private Button satellite, original, traffic;
+    private BaiduMap baiduMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        BMapManager.init();
         setContentView(R.layout.activity_main);
         initView();
-        initData();
-    }
-
-    private void initData() {
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arr);
-        spinner.setAdapter(adapter);
     }
 
     private void initView() {
-        spinner = (Spinner) findViewById(R.id.spinner);
+        satellite = (Button) findViewById(R.id.satellite);
+        original = (Button) findViewById(R.id.original);
+        traffic = (Button) findViewById(R.id.traffic);
+        mapView = (MapView) findViewById(R.id.bmapView);
+        baiduMap = mapView.getMap();
+        MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
+        baiduMap.setMapStatus(msu);
+
+        satellite.setOnClickListener(this);
+        original.setOnClickListener(this);
+        traffic.setOnClickListener(this);
+
     }
 
-    public void PopMenu(View view){
-        View view1 = LayoutInflater.from(MainActivity.this).inflate(R.layout.menu_i, null, false);
-        Button btn1 = view1.findViewById(R.id.btn1);
-        Button btn2 = view1.findViewById(R.id.btn2);
-        PopupWindow popw = new PopupWindow(view1, ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT,true);
-        popw.setTouchable(true);
-        popw.setBackgroundDrawable(new ColorDrawable(0x00000000));
-        popw.showAsDropDown(view,100,0);
-    }
 
-    public void Dialog(View view){
-        registerForContextMenu(view);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.my_menu,menu);
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
 
     @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.edit){
-            Toast.makeText(MainActivity.this,"1",Toast.LENGTH_SHORT).show();
-        }if(item.getItemId() == R.id.share){
-            Toast.makeText(MainActivity.this,"2",Toast.LENGTH_SHORT).show();
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.satellite:
+                baiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.original:
+                baiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.traffic:
+                if (baiduMap.isTrafficEnabled()) {
+                    baiduMap.setTrafficEnabled(false);
+
+                } else {
+                    baiduMap.setTrafficEnabled(true);
+                }
+                break;
         }
-        return super.onContextItemSelected(item);
     }
 }
